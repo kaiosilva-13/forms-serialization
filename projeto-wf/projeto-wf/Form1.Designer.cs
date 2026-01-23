b5ca7610ff48f56426c36c066b68d00bbfdfbd0f
kaiosilva-13 <kaiovitor13052005@gmail.com>
2026-01-23 10:25:13 -0300

    Menu de opções com MenuStrip adicionado, configurações da tela principal adicionadas

--- projeto-wf/projeto-wf/Form1.Designer.cs
+++ projeto-wf/projeto-wf/Form1.Designer.cs
@@ -28,12 +28,80 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            menuStrip1 = new MenuStrip();
+            opçõesToolStripMenuItem = new ToolStripMenuItem();
+            adicionarUsuárioToolStripMenuItem = new ToolStripMenuItem();
+            editarUsuárioToolStripMenuItem = new ToolStripMenuItem();
+            removerUsuárioToolStripMenuItem = new ToolStripMenuItem();
+            testarConexãoToolStripMenuItem = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { opçõesToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(559, 24);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // opçõesToolStripMenuItem
+            // 
+            opçõesToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { adicionarUsuárioToolStripMenuItem, editarUsuárioToolStripMenuItem, removerUsuárioToolStripMenuItem, testarConexãoToolStripMenuItem });
+            opçõesToolStripMenuItem.Name = "opçõesToolStripMenuItem";
+            opçõesToolStripMenuItem.Size = new Size(59, 20);
+            opçõesToolStripMenuItem.Text = "Opções";
+            // 
+            // adicionarUsuárioToolStripMenuItem
+            // 
+            adicionarUsuárioToolStripMenuItem.Name = "adicionarUsuárioToolStripMenuItem";
+            adicionarUsuárioToolStripMenuItem.Size = new Size(180, 22);
+            adicionarUsuárioToolStripMenuItem.Text = "&Adicionar Usuário";
+            // 
+            // editarUsuárioToolStripMenuItem
+            // 
+            editarUsuárioToolStripMenuItem.Name = "editarUsuárioToolStripMenuItem";
+            editarUsuárioToolStripMenuItem.Size = new Size(180, 22);
+            editarUsuárioToolStripMenuItem.Text = "&Editar Usuário";
+            // 
+            // removerUsuárioToolStripMenuItem
+            // 
+            removerUsuárioToolStripMenuItem.Name = "removerUsuárioToolStripMenuItem";
+            removerUsuárioToolStripMenuItem.Size = new Size(180, 22);
+            removerUsuárioToolStripMenuItem.Text = "&Remover Usuário";
+            // 
+            // testarConexãoToolStripMenuItem
+            // 
+            testarConexãoToolStripMenuItem.Name = "testarConexãoToolStripMenuItem";
+            testarConexãoToolStripMenuItem.Size = new Size(180, 22);
+            testarConexãoToolStripMenuItem.Text = "&Testar conexão";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(559, 371);
+            Controls.Add(menuStrip1);
+            FormBorderStyle = FormBorderStyle.FixedDialog;
+            MainMenuStrip = menuStrip1;
+            MaximizeBox = false;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Gerenciados de Usuários de Rede";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem opçõesToolStripMenuItem;
+        private ToolStripMenuItem adicionarUsuárioToolStripMenuItem;
+        private ToolStripMenuItem editarUsuárioToolStripMenuItem;
+        private ToolStripMenuItem removerUsuárioToolStripMenuItem;
+        private ToolStripMenuItem testarConexãoToolStripMenuItem;
     }
 }
